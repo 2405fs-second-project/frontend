eba27ae11cbdc50be65ab66d92ef5d14b1f95003
jiyoung choi <codingold@naver.com>
2024-07-18 14:52:29 +0900

    changes productlist

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,9 +7,14 @@ export const ProductList = ({ products }) => {
       <ul>
         {products.map((product, index) => (
           <li key={index}>
-            <strong>성별:</strong> {product.gender}, <strong>종류:</strong> {product.kind}, <strong>이름:</strong>{" "}
-            {product.name}, <strong>색상:</strong> {product.color}, <strong>풀네임:</strong> {product.full_name},{" "}
-            <strong>코드:</strong> {product.code}, <strong>재고:</strong> {product.stock}
+            <strong>성별:</strong> {product.gender}
+            <br />, <strong>종류:</strong> {product.kind}
+            <br />, <strong>이름:</strong> {product.name}
+            <br />, <strong>색상:</strong> {product.color}
+            <br />, <strong>풀네임:</strong> {product.full_name}
+            <br />,<strong>코드:</strong> {product.code}
+            <br />, <strong>재고:</strong> {product.stock}
+            <br />
           </li>
         ))}
       </ul>
